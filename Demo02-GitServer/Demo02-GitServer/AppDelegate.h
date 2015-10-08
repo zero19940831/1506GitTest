1e0a801235b88fd9a90a7fdacf9cd40b091c474e
tarena <tarena@tarenadeiMac-2.local>
2015-10-08 11:58:30 +0800

    sadsadas

---
+++ Demo02-GitServer/Demo02-GitServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo02-GitServer
+//
+//  Created by tarena on 15/10/8.
+//  Copyright (c) 2015年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
